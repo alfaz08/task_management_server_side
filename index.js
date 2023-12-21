a48d59139033383ddf8235e793aa2e41b09164ec
Alfaz Hossain <alfaz.cse@gmail.com>
2023-12-21 13:35:40 +0600

    users added to database using post

--- index.js
+++ index.js
@@ -31,7 +31,18 @@ async function run() {
 
     const userCollection = client.db("taskManagementDB").collection("users")
 
-
+  //users related api
+  app.post('/users',async(req,res)=>{
+    const user= req.body;
+    //insert email if user does not exist
+    const query = {email: user.email}
+    const existingUser = await userCollection.findOne(query)
+    if(existingUser){
+      return res.send({message: 'user already exist',insertedId:null})
+    }
+    const result = await userCollection.insertOne(user)
+    res.send(result)
+   })
 
 
 
